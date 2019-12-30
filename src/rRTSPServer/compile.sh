b00be1cbf9971c0ccc8f5c8cfda517e9b175be8d
Francesco <grausof@gmail.com>
2019-12-30 22:10:24 +0100

    Add script

--- src/rRTSPServer/compile.sh
+++ src/rRTSPServer/compile.sh
@@ -3,6 +3,6 @@ cd live || exit 1
 
 make clean
 make || exit 1
-cp ./rRTSPServer ../../bin/ || exit 1
+cp ./rRTSPServer ../../../bin/ || exit 1
 
-arm-linux-gnueabihf-strip ../../bin/* || exit 1
+arm-linux-gnueabihf-strip ../../../bin/* || exit 1
