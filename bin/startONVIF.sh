ef008f138c26d22fc58d132925053acc9fce6b45
Francesco <grausof@gmail.com>
2021-08-26 09:31:49 +0200

    Add ONVIF server

---
+++ bin/startONVIF.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/lib
+
+./onvif_srvd --model KEYKE --manufacturer KEYKE --firmware_ver 0.1.7 --hardware_id UffX --serial_num XXX --ifs eth0 --port 80 --scope onvif://www.onvif.org/Profile/S --name Profile_0 --width 640 --height 360 --url rtsp://%s/ch0_0.h264 --type H264
